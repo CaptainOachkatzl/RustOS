17506e74424253ddb846f17e45a00e387bd6d5f1
CaptainOachkatzl <dave.hofer@hotmail.com>
2020-06-10 06:56:55 +0200

    confirming string formatting macro still works

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -8,14 +8,18 @@ mod vga_driver;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! 
+{
     loop {}
 }
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-
+pub extern "C" fn _start() -> ! 
+{
+    use core::fmt::Write;
+    
     vga_driver::WRITER.lock().write_string("test");
+    write!(vga_driver::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
     loop {}
 }
