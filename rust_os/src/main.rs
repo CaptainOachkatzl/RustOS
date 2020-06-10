096d9bb3d74fa44caf0ccb3ebbbcf738cfc1d083
CaptainOachkatzl <dave.hofer@hotmail.com>
2020-06-10 07:46:10 +0200

    added startup messages

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -47,6 +47,10 @@ fn test_runner(tests: &[&dyn Fn()]) {
 #[no_mangle]
 pub extern "C" fn _start() -> ! 
 {
+    println!("Starting up OS...");
+
+    println!("Started.");
+
     #[cfg(test)]
     test_main();
 
