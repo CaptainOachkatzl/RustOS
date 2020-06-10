bb5b298351fe9540ce2709e6ea0a3d8403a7bcc5
CaptainOachkatzl <dave.hofer@hotmail.com>
2020-06-10 23:45:15 +0200

    setting entry point of bootloader with macro

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -7,25 +7,12 @@
 
 use core::panic::PanicInfo;
 use rust_os::println;
+use bootloader::{BootInfo, entry_point};
 
-/// This function is called on panic
-#[cfg(not(test))] 
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! 
-{
-    println!("{}", info);
-    rust_os::hlt_loop();
-}
-
-// panic handler in test mode
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    rust_os::test_panic_handler(info);
-}
+entry_point!(kernel_main);
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! 
+fn kernel_main(_boot_info: &'static BootInfo) -> ! 
 {
     println!("Starting up OS...");
 
@@ -41,4 +28,20 @@ pub extern "C" fn _start() -> !
     rust_os::exit_qemu(rust_os::QemuExitCode::Success);
 
     rust_os::hlt_loop();
+}
+
+/// This function is called on panic
+#[cfg(not(test))] 
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! 
+{
+    println!("{}", info);
+    rust_os::hlt_loop();
+}
+
+// panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info);
 }
