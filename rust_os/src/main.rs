e758c7473f7d1fcf21f5bb4bd203c4b3100933d4
CaptainOachkatzl <dave.hofer@hotmail.com>
2020-06-11 18:10:19 +0200

    intializing heap and replaced dummy allocator

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -5,10 +5,14 @@
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
 use core::panic::PanicInfo;
 use rust_os::println;
 use bootloader::{BootInfo, entry_point};
 #[cfg(test)] use rust_os::qemu::{exit_qemu, QemuExitCode};
+use rust_os::memory::{self, BootInfoFrameAllocator};
+use x86_64::VirtAddr;
 
 entry_point!(kernel_main);
 
@@ -21,7 +25,20 @@ fn kernel_main(boot_info: &'static BootInfo) -> !
 
     rust_os::init();
 
-    memory_mapping(boot_info);
+    //memory_mapping(boot_info);
+
+    use rust_os::allocator; 
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
+
+    allocate_memory();
 
     test_process();
 
@@ -42,6 +59,7 @@ fn test_process()
     exit_qemu(QemuExitCode::Success);
 }
 
+#[allow(dead_code)]
 fn memory_mapping(boot_info: &'static BootInfo)
 {
     use rust_os::memory;
@@ -62,6 +80,29 @@ fn memory_mapping(boot_info: &'static BootInfo)
     // write the string `New!` to the screen through the new mapping
     let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
     unsafe { page_ptr.offset(200).write_volatile(0x_f021_f077_f065_f04e)};
+}
+
+fn allocate_memory() 
+{
+    use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+    
+    // allocate a number on the heap
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 }
 
 /// This function is called on panic
