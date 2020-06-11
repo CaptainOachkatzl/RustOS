8cb572a7898ae0cd7e1f8d86ce3566d36483d357
CaptainOachkatzl <dave.hofer@hotmail.com>
2020-06-11 14:05:25 +0200

    making the OS more friendly :)

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -24,36 +24,17 @@ fn kernel_main(boot_info: &'static BootInfo) -> !
 
     rust_os::init();
 
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    // new: initialize a mapper
-    let mapper = unsafe { memory::init(phys_mem_offset) };
-
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
-
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        // new: use the `mapper.translate_addr` method
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
-
-
     #[cfg(test)]
     test_main();
     #[cfg(test)]
     exit_qemu(QemuExitCode::Success);
 
-    println!("Tasks finished. Going idle.");
+    kernel_process();
+}
 
+fn kernel_process() -> !
+{
+    println!("OS ready. Hello. :)");
     rust_os::hlt_loop();
 }
 
