77546e85a8afd058ee9a5125d2757c68bca05727
CaptainOachkatzl <dave.hofer@hotmail.com>
2020-06-11 23:46:16 +0200

    removed unused code

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -36,8 +36,6 @@ fn kernel_main(boot_info: &'static BootInfo) -> !
     allocator::init_heap(&mut mapper, &mut frame_allocator)
         .expect("heap initialization failed");
 
-    allocate_memory();
-
     test_process();
 
     kernel_process();
@@ -55,29 +53,6 @@ fn test_process()
     test_main();
     #[cfg(test)]
     exit_qemu(QemuExitCode::Success);
-}
-
-fn allocate_memory() 
-{
-    use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
-
-    // allocate a number on the heap
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    // create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
-    core::mem::drop(reference_counted);
-    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 }
 
 /// This function is called on panic
