5ceb2699bf924aff14200db32b357e2016cdd231
CaptainOachkatzl <dave.hofer@hotmail.com>
2020-06-12 08:18:10 +0200

    added executor/scheduler to run tasks when they are ready

--- rust_os/src/lib.rs
+++ rust_os/src/lib.rs
@@ -4,9 +4,10 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
-#![feature(alloc_error_handler)] // at the top of the file
+#![feature(alloc_error_handler)]
 #![feature(const_fn)]
 #![feature(const_in_array_repeat_expressions)]
+#![feature(wake_trait)]
 
 extern crate alloc;
 
